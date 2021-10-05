d013fe1d0d602900c9ed600117e4664a76edd7e4
seoltaehyeok <83220871+seoltaehyeok@users.noreply.github.com>
2021-10-05 16:49:49 +0900

    drawing board ( feat. nomadcoder )

--- nomadcoder/app.js
+++ nomadcoder/app.js
@@ -2,17 +2,34 @@ const canvas = document.getElementById("jsCanvas");
 const colorSet = document.getElementById("jsReset");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const button = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
+const INITIAL_COLOR = "#2c2c2c";
+// css의 캔버스 크기가 700이므로 width와 height의 크기또한 700으로 주면 된다.
+// canvas.width = 700;
+// canvas.height = 700;
+// 하지만 밑의 코드처럼 직접 캔버스의 너비와 높이를 가져오는 코드를 작성해도 된다.
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
-ctx.strokeStyle = "#2c2c2c";
+
+//not code & not fillColor drawing image save => backColor : transparentColor
+//Because default backColor -> white
+ctx.fillStyle = "white";
+ctx.fillRect(1, 1, canvas.width, canvas.height);
+
+//default fillColor, lineColor -> black
+ctx.fillStyle = INITIAL_COLOR
+ctx.strokeStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
+
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-
+    
     if(!painting) {
         ctx.beginPath();    // 경로 생성
         ctx.moveTo(x, y);   // 선 시작 좌표
@@ -23,13 +40,20 @@ function onMouseMove(event) {
     }
 }
 
-let painting = false; // 페인팅 기본값
+let painting = false; // painting default
+let filling = false; // Fill button default(★)
 
-function stopPainting(event) {
+function stopPainting() {
     painting = false;
 }
 
-function startPainting() {
+//mousedown left, right, wheel operate
+//only left click operate code
+function startPainting(event) {
+    if(event.which !=1){
+    return false;
+    }
+    else
     painting = true;
 }
 
@@ -38,10 +62,52 @@ function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     console.log(color);
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
+//새로고침(리셋)
 function colorReset() {
     window.location.reload()
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    console.log(size);
+    ctx.lineWidth = size;
+}
+
+// 처음 기본값이 false 이므로 버튼을 클릭할 때 else문이 동작
+// filling 은 true가 되고 버튼의 텍스트는 Paint가 된다.
+// 다시 클릭하게 되면 filling 은 true이므로 if문이 동작
+// 동작과 동시에 filling은 false가 되면서 버튼의 텍스트는 Fill이 된다.
+// 위 과정 반복
+function handleButtonClick(){
+    if(filling === true) {
+        filling = false;
+        button.innerText = "Fill"
+    } else {
+        filling = true;
+        button.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick(){
+    if(filling === true)
+        ctx.fillRect(1, 1, canvas.width, canvas.height);
+}
+
+// contextmenu 자체는 우클릭을 허용하지만 preventDefault();를 사용하는 즉시 우클릭이 금지된다.
+function handleCM(event){
+    event.preventDefault();
+}
+
+function saveImage(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;  // a href = 이미지 주소
+    link.download = "bySeol.png"; //파일이름
+    link.click();
+    
 }
 
 if(canvas){
@@ -49,6 +115,8 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -57,3 +125,14 @@ Array.from(colors).forEach(color =>
 
 colorSet.addEventListener("click", colorReset);
 
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(button){
+    button.addEventListener("click", handleButtonClick);
+}
+
+if(save){
+    save.addEventListener("click", saveImage);
+}
